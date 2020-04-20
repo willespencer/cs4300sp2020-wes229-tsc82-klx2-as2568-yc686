08b133d5c5942a6e3a3768a559c0807573a17090
yc686 <yc686@cornell.edu>
2020-04-20 13:40:37 -0400

    search_controller minor change

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,8 @@
 from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from app.irsystem.models.podcasts import Podcasts
+from app.irsystem.models.reviews import Reviews
 
 project_name = "Find the Pea to your Podcast"
 net_id = "Will Spencer: wes229, Theresa Cho: tsc82, Kathleen Xu: klx2, Yvonne Chan: yc686, Akira Shindo: as2568"
@@ -12,6 +14,7 @@ def search():
 		data = []
 		output_message = ''
 	else:
+		result = Podcasts.query.filter_by(name = 'Fresh Air').first()
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
