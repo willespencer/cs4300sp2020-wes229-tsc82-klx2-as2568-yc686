87864d974eec8770a67c901cbd5b93d16da5a22c
Theresa <tsc82@cornell.edu>
2020-04-20 13:45:22 -0400

    Uploaded placeholder image and updated dummy data schema

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -16,28 +16,26 @@ def search():
 	else:
 		output_message = "Your search: " + query
 		data_dict_list = [{
-		"pic": "myth.jpg",
+		"pic": "http://is1.mzstatic.com/image/thumb/Music118/v4/8e/52/e1/8e52e12c-1bf4-0d48-8aeb-97d7a0c55582/source/100x100bb.jpg",
 		"name": "Myths and Legends",
-		"author": "Anne",
 		"description": "Jason Weiser tells stories from myths, legends, and folklore that have shaped cultures throughout history. Some, like the stories of Aladdin, King Arthur, and Hercules are stories you think you know, but with surprising origins. Others are stories you might not have heard, but really should. All the stories are sourced from world folklore, but retold for modern ears. These are stories of wizards, knights, Vikings, dragons, princesses, and kings from the time when the world beyond the map was a dangerous and wonderful place.",
 		"episode_count": "40",
 		"avg_episode_duration": "20",
 		"link": "https://www.stitcher.com/podcast/jason-weiser/myths-and-legnen",
 		"similarity": "99",
-		"iTunes": "4.0",
+		"rating": "4.0",
 		"genres": ["Literature", "Fantasy"],
-		"similarities": [("Duration", "80"), ("No. Episodes", "100"), ("Genre", "100"), ("Description", "100")]
+		"similarities": [("Duration", "TBD"), ("No. Episodes", "TBD"), ("Genre", "TBD"), ("Description", "100")]
 	},
 	{
-		"pic": "coffee.jpg",
+		"pic": "placeholder.jpg",
 		"name": "Coffee",
-		"author": "Joe",
 		"description": "A podcast about coffee",
 		"episode_count": "100",
 		"avg_episode_duration": "15",
 		"link": "https://www.stitcher.com/podcast/studio71/coffee-talk-2",
 		"similarity": "5",
-		"iTunes": "1.5",
+		"rating": "1.5",
 		"genres": ["Food"],
 		"similarities": [("Duration", "15"), ("No. Episodes", "10"), ("Description", "0")]
 	}]
