5a728367e9be411976ede0d40b51760cb2dbb71a
Theresa <tsc82@cornell.edu>
2020-04-20 20:19:20 -0400

    Made query_dict and passed to get_ranked_podcast. Added all_reviews to get_ranked_podcast call

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -21,7 +21,6 @@ def search():
 
 	podcast = request.args.get('podcast_search')
 
-	#TODO: set podcast_names = all_podcast_names
 	podcast_names = all_podcast_names
 	
 	if not podcast:
@@ -49,8 +48,24 @@ def search():
 		# print(all_podcasts[0])
 
 		#replace 'Fresh Air' with podcast once the all_podcast_names are intergrated as valid inputs
-		query_podcast_info = Podcasts.query.filter_by(name='Fresh Air').first_or_404()
+		# query_podcast_info = Podcasts.query.filter_by(name='Fresh Air').first_or_404()
 		query_reviews = Reviews.query.filter_by(pod_name='Fresh Air').all()
+
+		query_podcast_info = Podcasts.query.filter_by(name=query).first_or_404()
+		query_dict = {
+			'name': query_podcast_info.name,
+			'description': query_podcast_info.description,
+			'episode_count': query_podcast_info.ep_count,
+			'avg_episode_duration': query_podcast_info.ep_durations,
+			'link': query_podcast_info.itunes_url,
+			'rating': query_podcast_info.rating
+		}
+		if query_podcast_info.artwork != None:
+			query_dict['pic'] = query_podcast_info.artwork
+		else:
+			query_dict['pic'] = "placeholder.jpg"
+			
+		query_dict['genres'] = (query_podcast_info.genres).split(';')
 
 		#formatting list of podcast reviews dicts for query
 		all_reviews = []
@@ -64,31 +79,31 @@ def search():
 			all_reviews.append(review_dict)
 		# print(all_reviews[0])
 
-		# # calculates similarity scores
-		# # data_dict_list = get_ranked_podcast(query, all_podcasts)
+		# calculates similarity scores
+		# data_dict_list = get_ranked_podcast(query_dict, all_podcasts, all_reviews)
 
-		data_dict_list = [{
-		"pic": "http://is1.mzstatic.com/image/thumb/Music118/v4/8e/52/e1/8e52e12c-1bf4-0d48-8aeb-97d7a0c55582/source/100x100bb.jpg",
-		"name": "Myths and Legends",
-		"description": "Jason Weiser tells stories from myths, legends, and folklore that have shaped cultures throughout history. Some, like the stories of Aladdin, King Arthur, and Hercules are stories you think you know, but with surprising origins. Others are stories you might not have heard, but really should. All the stories are sourced from world folklore, but retold for modern ears. These are stories of wizards, knights, Vikings, dragons, princesses, and kings from the time when the world beyond the map was a dangerous and wonderful place.",
-		"episode_count": "40",
-		"avg_episode_duration": "20",
-		"link": "https://www.stitcher.com/podcast/jason-weiser/myths-and-legnen",
-		"similarity": "99",
-		"rating": "4.0",
-		"genres": ["Literature", "Fantasy"],
-		"similarities": [("Duration", "TBD"), ("No. Episodes", "TBD"), ("Genre", "TBD"), ("Description", "100")]},
-		{
-		"pic": "placeholder.jpg",
-		"name": "Coffee",
-		"description": "A podcast about coffee",
-		"episode_count": "100",
-		"avg_episode_duration": "15",
-		"link": "https://www.stitcher.com/podcast/studio71/coffee-talk-2",
-		"similarity": "5",
-		"rating": "1.5",
-		"genres": ["Food"],
-		"similarities": [("Duration", "15"), ("No. Episodes", "10"), ("Description", "0")]
-		}]
+		# data_dict_list = [{
+		# "pic": "http://is1.mzstatic.com/image/thumb/Music118/v4/8e/52/e1/8e52e12c-1bf4-0d48-8aeb-97d7a0c55582/source/100x100bb.jpg",
+		# "name": "Myths and Legends",
+		# "description": "Jason Weiser tells stories from myths, legends, and folklore that have shaped cultures throughout history. Some, like the stories of Aladdin, King Arthur, and Hercules are stories you think you know, but with surprising origins. Others are stories you might not have heard, but really should. All the stories are sourced from world folklore, but retold for modern ears. These are stories of wizards, knights, Vikings, dragons, princesses, and kings from the time when the world beyond the map was a dangerous and wonderful place.",
+		# "episode_count": "40",
+		# "avg_episode_duration": "20",
+		# "link": "https://www.stitcher.com/podcast/jason-weiser/myths-and-legnen",
+		# "similarity": "99",
+		# "rating": "4.0",
+		# "genres": ["Literature", "Fantasy"],
+		# "similarities": [("Duration", "TBD"), ("No. Episodes", "TBD"), ("Genre", "TBD"), ("Description", "100")]},
+		# {
+		# "pic": "placeholder.jpg",
+		# "name": "Coffee",
+		# "description": "A podcast about coffee",
+		# "episode_count": "100",
+		# "avg_episode_duration": "15",
+		# "link": "https://www.stitcher.com/podcast/studio71/coffee-talk-2",
+		# "similarity": "5",
+		# "rating": "1.5",
+		# "genres": ["Food"],
+		# "similarities": [("Duration", "15"), ("No. Episodes", "10"), ("Description", "0")]
+		# }]
 	return render_template('search.html', name=project_name, netid=net_id, data=data_dict_list, podcast_names=podcast_names, show_modal=False)
 
