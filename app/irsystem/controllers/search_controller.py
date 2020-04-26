aa977cf30d680005c3fc9308f6dac1d6a784a601
Theresa <tsc82@cornell.edu>
2020-04-26 12:27:41 -0400

    Input cleaning and feedback after form submit

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -10,11 +10,43 @@ from app.irsystem.controllers.query_db import *
 project_name = "Find the Pea to your Podcast"
 net_id = "Will Spencer: wes229, Theresa Cho: tsc82, Kathleen Xu: klx2, Yvonne Chan: yc686, Akira Shindo: as2568"
 
+def cleanGenreQuery(genre_query):
+    if genre_query:
+        return genre_query
+    else:
+        return None
+
+def cleanAvgEpDurationQuery(avg_ep_duration_query):
+    if avg_ep_duration_query:
+        max_ep_duration_query = int(avg_ep_duration_query[avg_ep_duration_query.index("-")+1:avg_ep_duration_query.index(" ")])
+        min_ep_duration_query = int(avg_ep_duration_query[:avg_ep_duration_query.index("-")])
+    else:
+        max_ep_duration_query = None
+        min_ep_duration_query = 0
+    
+    return (max_ep_duration_query, min_ep_duration_query)
+
+def cleanMinEpCountQuery(min_ep_count_query):
+    if min_ep_count_query:
+        return int(min_ep_count_query[:min_ep_count_query.index(" ")])
+    else:
+        return 0
 
 @irsystem.route('/', methods=['GET'])
 def search():
-    # user input query
-    query = request.args.get('podcast_search')
+    # uncleaned user input
+    query_uncleaned = request.args.get('podcast_search')
+    genre_query_uncleaned = request.args.get('genre_search')
+    avg_ep_duration_query_uncleaned = request.args.get('avg_ep_duration')
+    min_ep_count_query_uncleaned = request.args.get('min_ep_count')
+
+    # user inputs and cleaning. 
+    # Handles case if genre, avg_ep_duration, min_ep_count not inputted
+    query = query_uncleaned
+    genre_query = cleanGenreQuery(genre_query_uncleaned)
+    # avg_ep_duration_query is tuple (<max>, <min>)
+    avg_ep_duration_query = cleanAvgEpDurationQuery(avg_ep_duration_query_uncleaned)
+    min_ep_count_query = cleanMinEpCountQuery(min_ep_count_query_uncleaned)
 
     # Note: the order changes everytime it's queried for some reason
     podcast_names = getAllPodcastNames()
@@ -64,4 +96,9 @@ def search():
     if(found_query):
         data_dict_list.pop(index_of_podcast)
 
-    return render_template('search.html', name=project_name, netid=net_id, data=data_dict_list, podcast_names=podcast_names, genres=genres, avg_ep_durations=avg_ep_durations, min_ep_counts=min_ep_counts, show_modal=True)
+    return render_template('search.html', name=project_name, netid=net_id, 
+    data=data_dict_list, podcast_names=podcast_names, genres=genres, 
+    avg_ep_durations=avg_ep_durations, min_ep_counts=min_ep_counts, 
+    query_feedback=query_uncleaned, genre_feedback=genre_query_uncleaned,
+    avg_ep_duration_feedback=avg_ep_duration_query_uncleaned,
+    min_ep_count_feedback=min_ep_count_query_uncleaned, show_modal=True)
